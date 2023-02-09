921db848cd743fc3d11609e08e117532c759b69c
Ayca Bas <aycabas@microsoft.com>
2023-02-09 20:11:59 +0400

    ps

--- templates/dotnet-uwp-app-microsoft-graph/setup.ps1
+++ templates/dotnet-uwp-app-microsoft-graph/setup.ps1
@@ -1,3 +1,3 @@
 az login
-$appId = az ad app create --display-name "MSGraph console app" --public-client-redirect-uris "http://localhost" --query appId -o tsv
+$appId = az ad app create --display-name "MSGraph UWP app" --public-client-redirect-uris "https://login.microsoftonline.com/common/oauth2/nativeclient" --query appId -o tsv
 ((Get-Content -path Program.cs -Raw) -replace 'CLIENT_ID',$appId) | Set-Content -Path Program.cs
--- templates/dotnet-uwp-app-microsoft-graph/setup.sh
+++ templates/dotnet-uwp-app-microsoft-graph/setup.sh
@@ -1,3 +1,3 @@
 az login
-appId=$(az ad app create --display-name "MSGraph console app" --public-client-redirect-uris "http://localhost" --query appId -o tsv)
+appId=$(az ad app create --display-name "MSGraph UWP app" --public-client-redirect-uris "https://login.microsoftonline.com/common/oauth2/nativeclient" --query appId -o tsv)
 sed -i '' -e "s/CLIENT_ID/$appId/g" Program.cs
