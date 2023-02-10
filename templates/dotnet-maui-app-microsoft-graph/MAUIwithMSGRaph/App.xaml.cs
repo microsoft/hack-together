03e31c634dea8dc7d8ffc2a22e3a1e59b27d5d44
Ayca Bas <aycabas@microsoft.com>
2023-02-10 13:06:07 +0400

    MAUI

---
+++ templates/dotnet-maui-app-microsoft-graph/MAUIwithMSGRaph/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUIwithMSGRaph;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ templates/dotnet-maui-app-microsoft-graph/MAUIwithMSGRaph/GraphService.cs
@@ -0,0 +1,61 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MAUIwithMSGRaph
+{
+    internal class GraphService
+    {
+        private readonly string[] _scopes = new[] { "User.Read" };
+        private const string TenantId = "tenant-id-from-app-registration";
+        private const string ClientId = "client-id-from-app-registration";
+        private GraphServiceClient _client;
+
+        public GraphService()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            // assume Windows for this sample
+            if (OperatingSystem.IsWindows())
+            {
+                var options = new InteractiveBrowserCredentialOptions
+                {
+                    TenantId = TenantId,
+                    ClientId = ClientId,
+                    AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+                    RedirectUri = new Uri("https://login.microsoftonline.com/common/oauth2/nativeclient"),
+                };
+
+                InteractiveBrowserCredential interactiveCredential = new(options);
+                _client = new GraphServiceClient(interactiveCredential, _scopes);
+            }
+            else
+            {
+                // TODO: Add iOS/Android support
+            }
+
+
+        }
+
+        public async Task<User> GetMyDetailsAsync()
+        {
+            try
+            {
+                return await _client.Me.Request().GetAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading user details: {ex}");
+                return null;
+            }
+        }
+    }
+}
